c8dc8990569e06927238839f75c8d30975224538
katarina pereira <kpf.freire@gmail.com>
2023-04-12 22:16:26 -0300

    Add classe controlle

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -16,6 +16,28 @@ public class HelloController {
 	
 	@GetMapping("/nome")
 	public String nome() {
-		return "<b>Katarina</b>";
+		return "<h3 style=\"color:red;\">Katarina</h3>";
 	}
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+				
+		return "<h1>Lista de bsm's:</h1>\r\n"
+				+ "<h3 style=\"color:orange;\">Comunicação;<br> \r\n"
+				+ "<font style=\"color:brown;\">Mentalidade de Crescimento;</font> <br>\r\n"
+				+ "<font style=\"color:orange;\">Orientação ao Detalhe; </font> <br>\r\n"
+				+ "<font style=\"color:brown;\">Orientação ao Futuro; </font> <br>\r\n"
+				+ "<font style=\"color:orange;\">Persistência; </font> <br>\r\n"
+				+ "<font style=\"color:brown;\">Proatividade; </font> <br>\r\n"
+				+ "<font style=\"color:orange;\">Responsabilidade pessoal;</font> <br>\r\n"
+				+ "<font style=\"color:brown;\">Trabalho em equipe.</font> </h3>\r\n";
+	}
+	
+	@GetMapping("/obj")
+	public String objetivos() {
+		return "<h1 style=\"color:blue;\">Objetivos:</h1>\r\n"
+				+ "<h3>Tenho para essa semana o objetivo continuar dando o meu melhor no bootcamp, aprender muito do Spring, html, Mysql, Git, Github e também conseguir fazer revisão do que aprendi em java. <br>\r\n"
+				+ "Além disso, também quero continuar com uma boa comunicação e trabalho em equipe com meus amigos do projeto integrador, sempre desenvolvendo e aprimorando minhas habilidades comportamentais. Além disso, tenho como meta conseguir construir uma boa gestão de tempo! :) </h3>";
+	}
+	
 }
